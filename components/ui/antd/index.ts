be3a60d78109c8368f2f89118df7e04fb40a1630
luiso96ballagas <luiso96ballagas@gmail.com>
2025-12-16 16:35:49 -0400

    feat(ui): add barrel export for Ant Design components
    - Export all layout components: AppShellAntd, AppSider, AppHeaderAntd, etc.
    - Export navigation components: NavMenu, UserDropdown
    - Export form components: SearchBox
    - Export feedback components: ThemeSwitch, StorageCard
    - Export typography components: Text
    - Enables clean imports from @/components/ui/antd

---
+++ components/ui/antd/index.ts
@@ -0,0 +1,20 @@
+// Ant Design Layout Components
+export { AppShellAntd } from "./layout/AppShellAntd";
+export { AppSider } from "./layout/AppSider";
+export { AppHeaderAntd } from "./layout/AppHeaderAntd";
+export { MobileDrawer } from "./layout/MobileDrawer";
+export { MobileHeaderAntd } from "./layout/MobileHeaderAntd";
+
+// Ant Design Navigation Components
+export { NavMenu } from "./navigation/NavMenu";
+export { UserDropdown } from "./navigation/UserDropdown";
+
+// Ant Design Form Components
+export { SearchBox } from "./forms/SearchBox";
+
+// Ant Design Feedback Components
+export { ThemeSwitch } from "./feedback/ThemeSwitch";
+export { StorageCard } from "./feedback/StorageCard";
+
+// Ant Design Typography Components
+export { Text } from "./typography/Text";
